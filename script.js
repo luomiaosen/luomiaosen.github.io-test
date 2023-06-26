9a55d2242c238715ecd86a48790b72b0d791dc6c
MIAO <67302030+luomiaosen@users.noreply.github.com>
2023-06-27 03:18:15 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-$(".custom-carousel").owlCarousel({
-  autoWidth: true,
-  loop: true
-});
-$(document).ready(function () {
-  $(".custom-carousel .item").click(function () {
-    $(".custom-carousel .item").not($(this)).removeClass("active");
-    $(this).toggleClass("active");
+
+
+// 向给定ID的用户发起请求
+// axios.
+axios.get('https://jsonplaceholder.typicode.com/todos/1')
+  .then(function (response) {
+    // 处理成功情况
+    console.log(response.data);
+    document.getElementById("demo").innerHTML=response.data.title;
+  })
+  .catch(function (error) {
+    // 处理错误情况
+    console.log(error);
+  })
+  .then(function () {
+    // 总是会执行
   });
-});
+
